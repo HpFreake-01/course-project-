3b769f39d71244f86f9f4cb8a9b877d09a074c92
Maksym Chuprun <m307837@alunos.ipb.pt>
2021-11-25 06:02:45 +0000

    add ref for textarea and create function to show value of textarea

--- src/componets/Profile/MyPosts/MyPosts.js
+++ src/componets/Profile/MyPosts/MyPosts.js
@@ -4,13 +4,20 @@ import Post from './Post/Post';
 
 const MyPosts = (props) => {
 
-    let postElement = props.postsData.map( post => <Post post={post} />)
+    let postElement = props.postsData.map( post => <Post post={post} />);
+
+    let newPostElement = React.createRef();
+
+    let Click = () => {
+        let data = newPostElement.current.value;
+        alert(data);
+    }
 
     return (
         <div>
             <div>My Posts</div>
-            <textarea/>
-            <button>Send</button>
+            <textarea ref= {newPostElement}/>
+            <button onClick={ Click }>Send</button>
             <div>
                {postElement}
             </div>
